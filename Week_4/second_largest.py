b36305a4cf6acc382d8ab86b26c53a4b37af300d
Sachpreet Singh <sachpreet1511@gmail.com>
2026-03-02 19:29:58 +0530

    intial commit

---
+++ Week_4/second_largest.py
@@ -0,0 +1,16 @@
+numbers = []
+
+for i in range(10):
+    value = int(input(f"Enter the {i+1} number: "))
+    numbers.append(value)
+
+largest = numbers[0]
+second_largest = None
+
+for num in numbers:
+    if num > largest:
+        second_largest = largest
+        largest = num
+
+print(f"The decond largest number is {second_largest}")
+
